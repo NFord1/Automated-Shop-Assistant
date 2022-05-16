78524919a152dcc2dd0fca6fbc7fd3f6acfdb308
Nathan Ford <nfor6010@uni.sydney.edu.au>
2022-05-17 09:28:58 +1000

    Add files via upload
    Still need to work out scaling of data.
    Not sure how we are intending to align with calibration box.

---
+++ Laser_Calibration.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "laser.h"
+#include "simple_serial.h"
+#include "derivative.h"      /* derivative-specific definitions */
+#include "Laser_Calibration.h"
+
+// Scale data
+// align with calibration box
+
+void laserCalibration(unsigned long *sample){
+
+  char buffer[128];
+  
+  //Intended distance
+  int dist=1;
+
+  //Check if the distance to calibration box is correct
+  if (sample > dist){
+    //move foreward
+    sprintf(buffer, "Move Device Foreward\r\n");
+  
+    SerialOutputString(buffer, &SCI1);
+  
+  } else if (sample < dist){
+    //move back
+    sprintf(buffer, "Move Device Back\r\n");
+  
+    SerialOutputString(buffer, &SCI1);
+  
+  } else if (sample == dist){
+    //correct position
+    sprintf(buffer, "Calibrated: Correct Position\r\n");
+  
+    SerialOutputString(buffer, &SCI1);
+  }
+  
+}
+
+// Could use accelerometer or gyro to warn if moving wrong way 
+// if incorporation of another sensor is required
---
+++ Laser_Calibration.h
@@ -0,0 +1,8 @@
+#ifndef __Laser_Calibration_h
+#define __Laser_Calibration_h
+
+// calibrate device to the correct distance from grid
+void laserCalibration(unsigned long *sample);
+
+
+#endif
